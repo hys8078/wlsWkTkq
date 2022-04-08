fe5fcb5940cbdfef7d039052d06ed0cd9875efe0
hys8078 <hsy042900@gmail.com>
2022-04-08 19:03:15 +0900

    First
    집에서 했던거까지

---
+++ My project(0402_Platformer)/Assets/02.Scripts/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //아니 깃허브왜 안바뀌냐거ㅓㅓㅓㅓ
+        Debug.Log("아ㅣㅣㅣ니ㅣㅣㅣㅣㅣ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
